b56d1de687ebea90cabfeb30f95e992c6a545b20
CreamSerenus <cseanwyatt@gmail.com>
2022-11-05 02:08:45 +0800

    Surprise update
    1. Added onclicks to buttons to trigger added javascript code on desc #2
    2. Added javascript which changes content based on portrait clicked
    3. added sample text
    4. Added Noelle's, Bennett's, and Razor's Splash art
    To-do:
    1. Set button default click on the first portrait (being Nahida currently)
    666. Fucking kill me, this piece of shit, get fixed boi ahsfkjgljhdlghdgljgljadfa

---
+++ web/BannerPage/portrait.js
@@ -0,0 +1,39 @@
+function changeContent(evt, charName) {
+    var i, tabContent, tabLinks, rateUp, perOp;
+    if (charName == "Nahida") {
+        document.getElementById("display").src="assets/splash/5splash1.png";
+        rateUp = "ru1";
+        perOp = "po1";
+    }
+    else if (charName == "Noelle") {
+        document.getElementById("display").src="assets/splash/4splash1.png";
+        rateUp = "ru2";
+        perOp = "po2";
+    }
+    else if (charName == "Bennett") {
+        document.getElementById("display").src="assets/splash/4splash2.png";
+        rateUp = "ru3";
+        perOp = "po3";
+    }
+    else { //charName == "Razor"
+        document.getElementById("display").src="assets/splash/4splash3.png"
+        rateUp = "ru4";
+        perOp = "po4";
+    }
+    
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    tabLinks = document.getElementsByClassName("tab-links");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(rateUp).style.display = "block";
+    document.getElementById(perOp).style.display = "block";
+
+    evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
